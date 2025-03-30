0d44f26a9f83825c68149f1ecff77ed4c2f988dd
Mohi-th <mohithmohith880@gmail.com>
2025-03-30 10:47:34 +0530

    header import name updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
 import './App.css'
-import Header from './components/header'
+import Header from './components/Header'
 import HomePage from './pages/HomePage'
 import CoinPage from './pages/CoinPage'
 
