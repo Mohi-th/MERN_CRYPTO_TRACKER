b9ac483d47fc5df5a632e67c39536ddaf833b29f
Mohi-th <mohithmohith880@gmail.com>
2025-03-30 11:23:45 +0530

    header navigation updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { CryptoState } from '../context/CryptoContext'
+import { useNavigate } from 'react-router-dom';
 
 function Header() {
 
   const {currency,setCurrency}=CryptoState();
-  console.log("currency" , currency)
+
+  const navigate=useNavigate();
 
   return (
     <header className='flex justify-between bg-gray-900 py-3 items-center px-6 border-b-3 border-black'>
-      <h1 className='text-amber-300 text-3xl font-bold'>
+      <h1 onClick={()=>{navigate('/')}} className='text-amber-300 text-3xl font-bold'>
         Crypto hunter
       </h1>
       <select value={currency} onChange={(e)=>{
